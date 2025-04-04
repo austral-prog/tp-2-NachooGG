36e1ea56d1b4ad67ad2f28de688fad7bd99a7ef4
NachooGG <igomezgalissier@mail.austral.edu.ar>
2025-04-04 10:11:07 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -3,6 +3,3 @@ def earth():
     y = "Barbados"
     print(f"The result of {x} comes first in the dictionary than {y} is {str(x < y)}.")
     print(f"The result of {y} comes first in the dictionary than {x} is {str(y < x)}.")
-
-earth()
-
